87143a9c2e2305c5c94bae1f3f83617c016ac2c5
vinnietito <vincentnjuguna69@gmail.com>
2025-04-23 12:12:00 +0300

    Commits

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int c, i = 1;
+
+        do
+        {
+            c = 1;
+            do
+            {
+                Console.Write(c);
+                c++; // Corrected 'C++' to 'c++'
+            } while (c <= i); // Updated logic to print increasing values
+
+            Console.WriteLine();
+            i++;
+        } while (i <= 5);
+
+        Console.ReadLine();
+    }
+}
